9290ab279cdde9012b505e355a7e0fa30ff3f449
HuyKhanh309 <lieuhuykhanh@gmail.com>
2024-06-06 17:47:24 +0700

    thêm menu

---
+++ WebOnThiTracNghiem/src/main/java/com/example/WebOnThiTracNghiem/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.WebOnThiTracNghiem.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("Welcome","Web ôn thi trắc nghiệm");
+        model.addAttribute("userName","Nhom1");
+        return "home/index";
+    }
+    @GetMapping("/admin")
+    public String admin(Model model){
+        model.addAttribute("adminName","Admin");
+        return "Admin/Admin";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model){
+        model.addAttribute("userName","Nhom1");
+        return "User/User";
+    }
+}
