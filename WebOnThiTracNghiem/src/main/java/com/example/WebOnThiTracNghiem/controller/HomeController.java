5785bd15adbc1c179173ed1d2fddc83cb8deaa8e
HuyKhanh309 <lieuhuykhanh@gmail.com>
2024-06-25 11:54:00 +0700

    Thêm phân quyền vào layout

--- WebOnThiTracNghiem/src/main/java/com/example/WebOnThiTracNghiem/controller/HomeController.java
+++ WebOnThiTracNghiem/src/main/java/com/example/WebOnThiTracNghiem/controller/HomeController.java
@@ -10,19 +10,15 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("Welcome","Web ôn thi trắc nghiệm");
-        model.addAttribute("userName","Nhom1");
         return "Home/index";
     }
     @GetMapping("/admin")
     public String admin(Model model){
-        model.addAttribute("adminName","Admin");
         return "Admin/Admin";
     }
 
     @GetMapping("/user")
     public String user(Model model){
-        model.addAttribute("userName","Nhom1");
         return "Users/User";
     }
 
